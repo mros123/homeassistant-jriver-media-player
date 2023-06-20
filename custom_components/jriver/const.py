2a2eab4b05441b3f926ab9a725a7edfec68b1115
andreas <andreas@mross.pw>
2023-06-20 13:19:44 +0000

    rename component

--- custom_components/jriver/const.py
+++ custom_components/jriver/const.py
@@ -3,7 +3,7 @@ from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+NAME = "Jriver Media Center"
+DOMAIN = "jriver"
 VERSION = "0.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
